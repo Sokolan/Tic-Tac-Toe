6f07196c52d79e5b8aa80ba6fb30ae01e2872a3f
Sokolan <valerysokolan@gmail.com>
2023-04-13 19:02:55 +0300

    prettier

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ const gameBoard = (() => {
   const rows = 3;
   const culs = 3;
   let movesMade = 0;
-  const empty = 'Empty';
+  const empty = "Empty";
 
   const cleanBoard = () => {
     for (let i = 0; i < rows; i += 1) {
@@ -47,19 +47,29 @@ const gameBoard = (() => {
   const checkForWin = (sign) => {
     // check for a row win
     for (let row = 0; row < rows; row += 1) {
-      if (board[row][0] === sign && board[row][1] === sign && board[row][2] === sign) {
+      if (
+        board[row][0] === sign &&
+        board[row][1] === sign &&
+        board[row][2] === sign
+      ) {
         return true;
       }
     }
     // check for culomn win
     for (let cul = 0; cul < culs; cul += 1) {
-      if (board[0][cul] === sign && board[1][cul] === sign && board[2][cul] === sign) {
+      if (
+        board[0][cul] === sign &&
+        board[1][cul] === sign &&
+        board[2][cul] === sign
+      ) {
         return true;
       }
     }
     // check for diagonal win
-    if ((board[0][0] === sign && board[1][1] === sign && board[2][2] === sign)
-      || (board[2][0] === sign && board[1][1] === sign && board[0][2] === sign)) {
+    if (
+      (board[0][0] === sign && board[1][1] === sign && board[2][2] === sign) ||
+      (board[2][0] === sign && board[1][1] === sign && board[0][2] === sign)
+    ) {
       return true;
     }
 
@@ -67,7 +77,7 @@ const gameBoard = (() => {
     return false;
   };
 
-  const checkIfCellAvailable = (row, cul) => (board[row][cul] === empty);
+  const checkIfCellAvailable = (row, cul) => board[row][cul] === empty;
 
   const getCellSign = (row, cul) => board[row][cul];
 
@@ -116,17 +126,22 @@ const gameControl = (() => {
     };
   })();
 
-  const initiateGame = (playerOneName, playerTwoName, playerVsAi, playerOneStart) => {
+  const initiateGame = (
+    playerOneName,
+    playerTwoName,
+    playerVsAi,
+    playerOneStart
+  ) => {
     // player one 0, player two 1 in array
     board.cleanBoard();
     currPlayer = playerOneStart ? 0 : 1;
     firstPlayer = currPlayer;
-    gameState = 'undesided';
+    gameState = "undesided";
 
-    const playerOneSign = playerOneStart ? 'X' : 'O';
+    const playerOneSign = playerOneStart ? "X" : "O";
     const playerOne = player(playerOneSign, playerOneName, true);
 
-    const playerTwoSign = playerOneStart ? 'O' : 'X';
+    const playerTwoSign = playerOneStart ? "O" : "X";
     const playerTwo = player(playerTwoSign, playerTwoName, !playerVsAi);
 
     players.pop();
@@ -140,7 +155,7 @@ const gameControl = (() => {
   };
 
   const resetGame = () => {
-    gameState = 'undesided';
+    gameState = "undesided";
     board.cleanBoard();
     winner = null;
     currPlayer = firstPlayer;
@@ -150,13 +165,13 @@ const gameControl = (() => {
   };
 
   const makeMove = (row, cul) => {
-    if (gameState !== 'undesided' || board.getMovesMade() === 9) {
+    if (gameState !== "undesided" || board.getMovesMade() === 9) {
       return;
     }
 
     const moveWasMade = board.makeMove(players[currPlayer].getSign(), row, cul);
     if (!moveWasMade) {
-      console.log('Illegal parameters');
+      console.log("Illegal parameters");
       return;
     }
 
@@ -164,7 +179,7 @@ const gameControl = (() => {
 
     if (board.checkForWin(players[currPlayer].getSign())) {
       console.log(`${players[currPlayer].getName()} Wan!`);
-      gameState = 'wan';
+      gameState = "wan";
       winner = players[currPlayer];
       return;
     }
@@ -176,8 +191,8 @@ const gameControl = (() => {
     }
 
     if (board.getMovesMade() === 9) {
-      gameState = 'tie';
-      console.log('it\'s a tie!');
+      gameState = "tie";
+      console.log("it's a tie!");
     }
   };
 
@@ -207,13 +222,15 @@ const displayHandler = (() => {
   const game = gameControl;
 
   const updateCurrentTurn = () => {
-    document.querySelector('p.curr-turn').textContent = `${game.getCurrPlayerName()}'s Turn`;
+    document.querySelector(
+      "p.curr-turn"
+    ).textContent = `${game.getCurrPlayerName()}'s Turn`;
   };
 
   const updateWinner = (winner) => {
-    const winnerElement = document.querySelector('p.winner');
+    const winnerElement = document.querySelector("p.winner");
     winnerElement.textContent = winner;
-    winnerElement.style.display = 'block';
+    winnerElement.style.display = "block";
   };
 
   const updateDisplayedBoard = () => {
@@ -224,37 +241,36 @@ const displayHandler = (() => {
         const sign = board.getCellSign(row, cul);
         if (sign !== board.boardEmptySign()) {
           cell.textContent = sign;
-        }
-        else {
-          cell.textContent = '';
+        } else {
+          cell.textContent = "";
         }
       }
     }
     updateCurrentTurn();
     const gameState = game.getGameState();
-    if (gameState === 'tie') {
-      updateCurrentTurn('tie');
+    if (gameState === "tie") {
+      updateCurrentTurn("tie");
     }
-    if (gameState === 'wan') {
+    if (gameState === "wan") {
       updateWinner(`Player '${game.getWinner().getName()}' Wan!`);
     }
   };
 
   const userMoveHandler = (event) => {
-    const row = event.target.getAttribute('row');
-    const cul = event.target.getAttribute('cul');
+    const row = event.target.getAttribute("row");
+    const cul = event.target.getAttribute("cul");
     game.makeMove(row, cul);
     updateDisplayedBoard();
   };
 
   const createBoardDisplay = () => {
-    const container = document.querySelector('.board');
+    const container = document.querySelector(".board");
     for (let row = 0; row < 3; row += 1) {
       for (let cul = 0; cul < 3; cul += 1) {
-        const cellElement = document.createElement('div');
-        cellElement.setAttribute('row', `${row}`);
-        cellElement.setAttribute('cul', `${cul}`);
-        cellElement.addEventListener('click', userMoveHandler);
+        const cellElement = document.createElement("div");
+        cellElement.setAttribute("row", `${row}`);
+        cellElement.setAttribute("cul", `${cul}`);
+        cellElement.addEventListener("click", userMoveHandler);
         container.appendChild(cellElement);
       }
     }
@@ -264,37 +280,44 @@ const displayHandler = (() => {
   const resetGame = () => {
     game.resetGame();
     updateDisplayedBoard();
-    document.querySelector('.winner').style.display = 'none';
+    document.querySelector(".winner").style.display = "none";
   };
-  document.querySelector('.reset-game').addEventListener('click', resetGame);
+  document.querySelector(".reset-game").addEventListener("click", resetGame);
 
   const changeSettings = () => {
     resetGame();
-    document.querySelector('.board').replaceChildren();
+    document.querySelector(".board").replaceChildren();
     optionsScreen();
   };
 
-  document.querySelector('.change-settings').addEventListener('click', changeSettings);
+  document
+    .querySelector(".change-settings")
+    .addEventListener("click", changeSettings);
 
   const startGame = (event) => {
     event.preventDefault();
     // Switch between game options screen and board screen
-    document.querySelector('form').style.display = 'none';
-    document.querySelector('.board').style.display = 'grid';
-    document.querySelector('.new-game-buttons').style.display = 'flex';
-    document.querySelector('.game-info').style.display = 'flex';
+    document.querySelector("form").style.display = "none";
+    document.querySelector(".board").style.display = "grid";
+    document.querySelector(".new-game-buttons").style.display = "flex";
+    document.querySelector(".game-info").style.display = "flex";
 
-    let playerOneName = document.querySelector('#playerOneName').value;
-    if (playerOneName === '') {
-      playerOneName = 'Player One';
+    let playerOneName = document.querySelector("#playerOneName").value;
+    if (playerOneName === "") {
+      playerOneName = "Player One";
     }
-    let playerTwoName = document.querySelector('#playerTwoName').value;
-    if (playerTwoName === '') {
-      playerTwoName = 'Player Two';
+    let playerTwoName = document.querySelector("#playerTwoName").value;
+    if (playerTwoName === "") {
+      playerTwoName = "Player Two";
     }
-    const playAgainstAi = document.querySelector('#playAgainstAi').checked;
-    const playerOneStart = !(document.querySelector('#PlayerTwoStart').checked);
-    game.initiateGame(playerOneName, playerTwoName, playAgainstAi, playerOneStart);
+    const playAgainstAi = document.querySelector("#playAgainstAi").checked;
+    const playerOneStart = !document.querySelector("#PlayerTwoStart").checked;
+    game.initiateGame(
+      playerOneName,
+      playerTwoName,
+      playAgainstAi,
+      playerOneStart
+    );
     createBoardDisplay();
     if (playAgainstAi) {
       updateDisplayedBoard();
@@ -302,12 +325,12 @@ const displayHandler = (() => {
   };
 
   const optionsScreen = () => {
-    document.querySelector('.board').style.display = 'none';
-    document.querySelector('.new-game-buttons').style.display = 'none';
-    document.querySelector('.game-info').style.display = 'none';
-    document.querySelector('form').style.display = 'grid';
-    const startGameButton = document.querySelector('.start-game');
-    startGameButton.addEventListener('click', startGame);
+    document.querySelector(".board").style.display = "none";
+    document.querySelector(".new-game-buttons").style.display = "none";
+    document.querySelector(".game-info").style.display = "none";
+    document.querySelector("form").style.display = "grid";
+    const startGameButton = document.querySelector(".start-game");
+    startGameButton.addEventListener("click", startGame);
   };
 
   return {
